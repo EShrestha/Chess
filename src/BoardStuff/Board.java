a78f483eac67dd36bb2c14a22a3790a3c2c1e4c1
Erik Shrestha <EShrestha@Student.neumont.edu>
2020-08-06 16:39:00 -0600

    More board stuff

--- src/BoardStuff/Board.java
+++ src/BoardStuff/Board.java
@@ -1,8 +1,8 @@
 package BoardStuff;
 
 public class Board {
-    private final int dimension = 8;
-    Tile[][] board = new Tile[dimension][dimension];
+    private static final Integer DIMENSION = 8;
+    Tile[][] board = new Tile[DIMENSION][DIMENSION];
 
     // If a file is passed in, setup board using this
     public Board(String chessCommandFiles){
@@ -15,12 +15,28 @@ public class Board {
             int col = 0;
             TileColor currentColor = (i % 2 == 0) ? TileColor.LIGHT : TileColor.DARK;
             for(File file : File.values()){
-                Tile newTile = new Tile(currentColor, new Location(file, i));
+                Tile newTile = new Tile(currentColor, new Location(file, DIMENSION-i));
                 board[i][col] = newTile;
                 currentColor = (currentColor == TileColor.DARK) ? TileColor.LIGHT : TileColor.DARK;
                 col++;
             }
         }
 
+    }
+
+    public void printBoard(){
+        for(Tile[] row : board){
+
+            for(Tile tile : row){
+                if(tile.isHasPiece()){
+                    // this is where you would print the piece name, EX: | R | N | B | K | Q |...
+                    System.out.println("X");
+                }else{
+                    System.out.println("-");
+                }
+                System.out.println(); // Just a line separater
+            }
+
+        }
     }
 }
--- src/Controller/AppControl.java
+++ src/Controller/AppControl.java
@@ -1,11 +1,13 @@
 package Controller;
 
+import BoardStuff.Board;
 import Command.CommandReader;
 
 public class AppControl {
 
     public void run(){
-        new CommandReader().readCommandFromFile("chessCommands"); // .txt extension is implied
+        //new CommandReader().readCommandFromFile("chessCommands"); // .txt extension is implied
+        new Board().printBoard();
 
     }
 }
