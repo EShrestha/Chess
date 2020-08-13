128773fb8fcacc4cbbbdf74dac2e5a4b9d519aac
AnibalTR <aanibaltinoco@gmail.com>
2020-08-12 18:54:42 -0600

    Rook can now do rook things however it can jump over pieces.

--- src/Model/Rook.java
+++ src/Model/Rook.java
@@ -2,9 +2,13 @@ package Model;
 
 import BoardStuff.Board;
 import BoardStuff.Location;
+import BoardStuff.LocationGenerator;
 import BoardStuff.Tile;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Rook extends Piece implements Movable{
 
@@ -17,8 +21,36 @@ public class Rook extends Piece implements Movable{
 
     @Override
     public List<Location> getValidMoves(Board board) {
-        System.out.println("This method 'getValidMoves' needs to be implemented for the " + this.getName() + " class.");
-        return null;
+        List<Location> possibleMoveTiles = new LinkedList<>();
+        Location currentLocation = this.getCurrentTile().getLocation();
+
+        for (int p = 1; p < 8; p++) { // Lets Rook move up and to the right
+            possibleMoveTiles.add(LocationGenerator.build(currentLocation, 0, p));
+            possibleMoveTiles.add(LocationGenerator.build(currentLocation, p, 0));
+
+            for (int n = -1; n > -8; n--) { // Lets rook move down and left
+                possibleMoveTiles.add(LocationGenerator.build(currentLocation, 0, n));
+                possibleMoveTiles.add(LocationGenerator.build(currentLocation, n, 0));
+            }
+        }
+
+        Map<Location, Tile> tileMap = board.getLocationTileMap();
+
+        List<Location> validMoves = new LinkedList<>();
+
+        for(Location l : possibleMoveTiles) {
+            if (l != null && tileMap.get(l).isHasPiece()) {
+                if (tileMap.get(l).getCurrentPiece().getPieceColor() != this.getPieceColor()
+                        && tileMap.get(l).getLocation().getFile() != this.getCurrentTile().getLocation().getFile()) {
+                    validMoves.add(l);
+                }
+            } else if (l != null && !tileMap.get(l).isHasPiece()) {
+                if (tileMap.get(l).getLocation().getFile() == this.getCurrentTile().getLocation().getFile()) {
+                    validMoves.add(l);
+                }
+            }
+        }
+        return validMoves;
     }
 
     @Override
@@ -30,4 +62,6 @@ public class Rook extends Piece implements Movable{
     public void makeMove(Tile tile) {
         System.out.println("This method 'makeMove' needs to be implemented for the " + this.getName() + " class.");
     }
+
+    //
 }
