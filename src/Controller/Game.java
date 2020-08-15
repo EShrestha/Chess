84d06fcd835813eddd75366720770fb364dd3ef5
AnibalTR <aanibaltinoco@gmail.com>
2020-08-14 20:10:04 -0600

    game class restored

--- src/Controller/Game.java
+++ src/Controller/Game.java
@@ -3,13 +3,14 @@ package Controller;
 import BoardStuff.Board;
 import BoardStuff.Location;
 import BoardStuff.RankToRank;
-import BoardStuff.Tile;
-import Model.King;
+import Model.Pawn;
 import Model.Piece;
 import Model.Player;
 import lib.ConsoleIO;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,11 +22,7 @@ public class Game {
     public static int movesMade = 0;
     boolean lightResign = false;
     boolean darkResign = false;
-    Tile darkKingTile;
-    Tile lightKingTile;
-    boolean notGameOver = true;
     Board playingBoard;
-    private boolean playerResignState = true;
 
 
     public Game(){
@@ -33,6 +30,7 @@ public class Game {
 
     public void playGame(Board activeBoard){
         playingBoard = activeBoard;
+        boolean notGameOver = true;
         playingBoard.printBoard();
 
         do{
@@ -40,24 +38,21 @@ public class Game {
             if(movesMade % 2 == 0){ // Lights turn
 
                 System.out.print("LIGHT -> ");
-                    makeValidMove('l');
-                    if(!notGameOver){break;}
+                makeValidMove('l');
 
                 movesMade++;
 
-            }else if(movesMade % 2 == 1) { // Dark turn
+            }else if(movesMade % 2 == 1) { // Darks turn
 
                 System.out.print("DARK -> ");
                 makeValidMove('d');
-                if(!notGameOver){break;}
-
 
                 movesMade++;
 
             }
-
-            System.out.println();
-            refreshBoard();
+            System.out.println(darkPlayer.getCapturedPieces());
+            playingBoard.printBoard();
+            System.out.println(lightPlayer.getCapturedPieces());
 
         }while (notGameOver);
 
@@ -67,121 +62,68 @@ public class Game {
         // Group 1 2 3 4 ----> a 1  a 2
         Pattern p = Pattern.compile("^([a-h])(\\d)\\s([a-h])(\\d)$");
         Matcher m = p.matcher("");
-        boolean validMoveMade = false;
+        Boolean validMoveMade = false;
 
-        if(!m.matches()) {
-            String resign = ConsoleIO.promptForString("Enter piece coordinate and move to coordinate (a1 a2): ").toLowerCase();
-            if (resign.equals("q") || m.matches()) {
-                notGameOver = playerQuits(color);
+        do {
+            String move;
+            while (!m.matches()) {
+                m = p.matcher(ConsoleIO.promptForString("Enter piece coordinate and move to coordinate (a1 a2): "));
+                if (!m.matches()) {
+                    System.out.print(color == 'l' ? "LIGHT -> " : "DARK -> ");
+                }
+            }
+
+            // Getting x and y of the piece user wants ot move and the x and y of where user want to move piece to
+            Integer xCurrent = Enum.valueOf(BoardStuff.File.class, m.group(1).toUpperCase()).ordinal();
+            Integer yCurrent = rankToRank.getRank(Integer.parseInt(m.group(2)) - 1);
+            Integer xMoveTo = Enum.valueOf(BoardStuff.File.class, m.group(3).toUpperCase()).ordinal();
+            Integer yMoveTo = rankToRank.getRank(Integer.parseInt(m.group(4)) - 1);
+
+
+            if (playingBoard.board[yCurrent][xCurrent].isHasPiece()) {
+                // Getting a list of valid locations for the current piece
+                List<Location> validLocations = playingBoard.board[yCurrent][xCurrent].getCurrentPiece().getValidMoves(playingBoard);
+                System.out.println("VALID MOVES: " + validLocations);
+
+                // Checking if the piece color user wants to move is actually the players color (l/d) and the move they want to make is in the valid locations list
+                if (color == playingBoard.board[yCurrent][xCurrent].getCurrentPiece().getShortColor()
+                        && validLocations.contains(playingBoard.board[yMoveTo][xMoveTo].getLocation())) {
+
+                    // Marking the piece as already moved once
+                    playingBoard.board[yCurrent][xCurrent].getCurrentPiece().setFirstMove(false);
+
+                    // Making a copy of the piece user wants to move then resetting that tile the piece was on
+                    Piece tempCurrentPiece = playingBoard.board[yCurrent][xCurrent].getCurrentPiece();
+                    playingBoard.board[yCurrent][xCurrent].resetTile();
+
+                    // Adding what piece was captured if there was a piece to be captured
+                    if(playingBoard.board[yMoveTo][xMoveTo].isHasPiece()) {
+                        Piece tempMoveToPiece = playingBoard.board[yMoveTo][xMoveTo].getCurrentPiece();
+                        if(color == 'l'){ lightPlayer.addCapturedPiece(tempMoveToPiece);
+                        }else { darkPlayer.addCapturedPiece(tempMoveToPiece); }
+
+                    }
+
+                    playingBoard.board[yMoveTo][xMoveTo].setCurrentPiece(tempCurrentPiece);
+                    //Setting what tile the piece is on for the piece that just moved
+                    playingBoard.board[yMoveTo][xMoveTo].getCurrentPiece().setCurrentTile(playingBoard.board[yMoveTo][xMoveTo]);
+
+
+                    validMoveMade = true;
+                    move = "X";
+                } else {
+                    System.out.println("***INVALID MOVE***");
+                    System.out.print(color == 'l' ? "LIGHT -> " : "DARK -> ");
+                }
+
             }else {
-                do {
-
-                    while (!m.matches()) {
-                        System.out.print(color == 'l' ? "LIGHT -> " : "DARK -> ");
-                        m = p.matcher(ConsoleIO.promptForString("Enter piece coordinate and move to coordinate (a1 a2): ").toLowerCase());
-                        if (!m.matches()) {
-
-                        }
-                    }
-
-                    // Getting x and y of the piece user wants ot move and the x and y of where user want to move piece to
-                    int xCurrent = Enum.valueOf(BoardStuff.File.class, m.group(1).toUpperCase()).ordinal();
-                    int yCurrent = rankToRank.getRank(Integer.parseInt(m.group(2)) - 1);
-                    int xMoveTo = Enum.valueOf(BoardStuff.File.class, m.group(3).toUpperCase()).ordinal();
-                    int yMoveTo = rankToRank.getRank(Integer.parseInt(m.group(4)) - 1);
-
-
-                    if (playingBoard.board[yCurrent][xCurrent].isHasPiece()) {
-                        // Getting a list of valid locations for the current piece
-                        List<Location> validLocations = playingBoard.board[yCurrent][xCurrent].getCurrentPiece().getValidMoves(playingBoard);
-                        System.out.println("ACTUAL VALID MOVES: " + validLocations);
-
-                        // Checking if the piece color user wants to move is actually the players color (l/d) and the move they want to make is in the valid locations list
-                        if (color == playingBoard.board[yCurrent][xCurrent].getCurrentPiece().getShortColor()
-                                && validLocations.contains(playingBoard.board[yMoveTo][xMoveTo].getLocation())) {
-
-                            // Probably check for check/checkmate here
-
-                            // Marking the piece as already moved once
-                            playingBoard.board[yCurrent][xCurrent].getCurrentPiece().setFirstMove(false);
-
-                            // Making a copy of the piece user wants to move then resetting that tile the piece was on
-                            Piece tempCurrentPiece = playingBoard.board[yCurrent][xCurrent].getCurrentPiece();
-                            playingBoard.board[yCurrent][xCurrent].resetTile();
-
-                            // Adding what piece was captured if there was a piece to be captured
-                            if (playingBoard.board[yMoveTo][xMoveTo].isHasPiece()) {
-                                Piece tempMoveToPiece = playingBoard.board[yMoveTo][xMoveTo].getCurrentPiece();
-                                if (color == 'l') {
-                                    lightPlayer.addCapturedPiece(tempMoveToPiece);
-                                } else {
-                                    darkPlayer.addCapturedPiece(tempMoveToPiece);
-                                }
-
-                            }
-
-                            playingBoard.board[yMoveTo][xMoveTo].setCurrentPiece(tempCurrentPiece);
-                            //Setting what tile the piece is on for the piece that just moved
-                            playingBoard.board[yMoveTo][xMoveTo].getCurrentPiece().setCurrentTile(playingBoard.board[yMoveTo][xMoveTo]);
-
-                            validMoveMade = true;
-
-
-                        } else {
-                            System.out.println("***INVALID MOVE***");
-                            System.out.print(color == 'l' ? "LIGHT -> " : "DARK -> ");
-                            m = p.matcher("xx");
-                        }
-
-                    } else {
-                        System.out.println("***INVALID MOVE***");
-                        System.out.print(color == 'l' ? "LIGHT -> " : "DARK -> ");
-                        m = p.matcher("xx");
-                    }
-                    // Clearing user input so user can input again
-                    m = p.matcher("");
-                } while (!validMoveMade);
+                System.out.println("***INVALID MOVE***");
+                System.out.print(color == 'l' ? "LIGHT -> " : "DARK -> ");
             }
+            // Clearing user input so user can input again
+            m = p.matcher("");
+        }while (!validMoveMade) ;
 
-        }
 
     }
-
-
-    public void refreshBoard(){
-        System.out.print("CAPTURED PIECES: ");
-        for(Piece dP : darkPlayer.getCapturedPieces()){
-            System.out.print(dP.getShortName() + " ");
-        }
-        System.out.println("\n");
-        playingBoard.printBoard();
-        System.out.print("CAPTURED PIECES: ");
-        for(Piece lP : lightPlayer.getCapturedPieces()){
-            System.out.print(lP.getShortName() + " ");
-        }
-        System.out.println("\n");
-    }
-    private boolean gameContinue;
-    public boolean playerQuits(char playerColor){
-
-        while(playerResignState){
-            String response = ConsoleIO.promptForString("Are you sure you want to forfeit? (y/n): ").toUpperCase();
-            if(response.equals("Y")){
-                System.out.print(playerColor == 'l' ? "LIGHT forfeits DARK wins!!!" : "DARK forfeits LIGHT wins!!! ");
-                gameContinue = false;
-                playerResignState = false;
-            }
-            else if (response.equals("N")){
-                gameContinue = true;
-                playerResignState = false;
-            }
-            else{
-                System.out.println("Yes or No? (y/n): ");
-            }
-        }
-        return gameContinue;
-
-    }
-
-
 }
